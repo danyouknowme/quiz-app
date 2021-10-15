d05829b9264f07e0307c67f7f642c7af0fc6e266
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-15 12:11:04 +0700

    declare variable and passing to question card

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 // Components
 import QuestionCard from "./components/QuestionCard";
 
+const TOTAL_QUESTIONS = 10;
+
 const App = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [questions, setQuestions] = useState<string[]>([]);
+  const [number, setNumber] = useState<number>(0);
+  const [userAnswers, setUserAnswers] = useState<string[]>([]);
+  const [score, setScore] = useState<number>(0);
+  const [gameOver, setGameOver] = useState<boolean>(true);
 
   const startTravia = async () => {
 
@@ -24,7 +32,14 @@ const App = () => {
       </button>
       <p className="score">Score: </p>
       <p>Loading Question ...</p>
-      <QuestionCard />
+      <QuestionCard 
+        questionNumber={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
