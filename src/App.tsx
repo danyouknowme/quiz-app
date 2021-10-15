167d9e6dd7e40df706caf870d282e0ba282f33c9
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-15 13:55:26 +0700

    allow checkAnswer function to check answer and count score

--- src/App.tsx
+++ src/App.tsx
@@ -34,11 +34,26 @@ const App = () => {
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-
+    if (!gameOver) {
+      // Get user answer
+      const answer = e.currentTarget.value;
+      // Check answer against correct answer
+      const correct = questions[number].correct_answer === answer;
+      // Add score if answer is correct
+      if (correct) setScore(prev => prev + 1);
+      // Save answer in the array for user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers(prev => [...prev, answerObject]);
+    }
   }
 
   const nextQuestion = () => {
-
+    
   }
   
   return (
