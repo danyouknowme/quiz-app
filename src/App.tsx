d712015d2b6ef82667fd7e60b436d1c64da5605f
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-15 13:26:00 +0700

    fetching data API and testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { fetchQuizQuestions, Difficulty } from "./API";
 // Components
 import QuestionCard from "./components/QuestionCard";
 
@@ -11,6 +12,8 @@ const App = () => {
   const [userAnswers, setUserAnswers] = useState<string[]>([]);
   const [score, setScore] = useState<number>(0);
   const [gameOver, setGameOver] = useState<boolean>(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
 
   const startTravia = async () => {
 
@@ -32,14 +35,15 @@ const App = () => {
       </button>
       <p className="score">Score: </p>
       <p>Loading Question ...</p>
-      <QuestionCard 
+
+      {/* <QuestionCard 
         questionNumber={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      />
+      /> */}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
